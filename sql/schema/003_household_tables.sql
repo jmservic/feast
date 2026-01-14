3df777a971e569c74604bd94429279faf86bc054
Sacriflces <Jmhservice@gmail.com>
2026-01-14 16:11:28 -0500

    added the main household tables.

--- sql/schema/003_household_tables.sql
+++ sql/schema/003_household_tables.sql
@@ -4,13 +4,30 @@ CREATE TABLE households (
 	created_at TIMESTAMP NOT NULL, 
 	name TEXT NOT NULL
 );
+
+CREATE TABLE household_roles (
+	id SERIAL PRIMARY KEY,
+	name TEXT NOT NULL
+);
+
 CREATE TABLE household_members (
 	id UUID PRIMARY KEY,
 	name TEXT,
-	role INT, -- Foreign key
-	household_id UUID, --Foreign key
-	user_id UUID --Foreign key
+	created_at TIMESTAMP NOT NULL,
+	updated_at TIMESTAMP NOT NULL,
+	role INTEGER REFERENCES household_roles (id), 
+	household_id UUID REFERENCES households (id),
+	user_id UUID REFERENCES users (id)
 );
 
+-- initial data
+INSERT INTO household_roles (name) 
+VALUES
+	('administrator'),
+	('manager'),
+	('member');
+
 -- +goose Down
+DROP TABLE household_members;
+DROP TABLE household_roles;
 DROP TABLE households;
