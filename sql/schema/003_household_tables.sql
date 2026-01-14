d9058700ea28750b149186f97e5393dd166fa425
Sacriflces <Jmhservice@gmail.com>
2026-01-13 21:51:54 -0500

    Added more hosehold tables to the schema script

--- sql/schema/003_household_tables.sql
+++ sql/schema/003_household_tables.sql
@@ -4,6 +4,13 @@ CREATE TABLE households (
 	created_at TIMESTAMP NOT NULL, 
 	name TEXT NOT NULL
 );
+CREATE TABLE household_members (
+	id UUID PRIMARY KEY,
+	name TEXT,
+	role INT, -- Foreign key
+	household_id UUID, --Foreign key
+	user_id UUID --Foreign key
+);
 
 -- +goose Down
 DROP TABLE households;
