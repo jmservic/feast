75556152fe0fb5f3ddc9006ad6097aead47cfaad
Sacriflces <Jmhservice@gmail.com>
2026-01-19 12:21:14 -0500

    Added the initial unit test

--- integration_tests/users_test.go
+++ integration_tests/users_test.go
@@ -1,5 +1,27 @@
 package integration
 
 import (
-// "testing"
+	"bytes"
+	"net/http"
+	"os"
+	"testing"
 )
+
+func TestCreateNewUser(t *testing.T) {
+	//loadDotEnv()
+	feast_url := "http://localhost:" + os.Getenv("PORT")
+	body := bytes.NewReader([]byte(`{
+		"name": "jonathan",
+		"email":  "jon@example.com",
+		"password": "very-secret!"
+	}`))
+
+	res, err := http.Post(feast_url+"/api/users", "application/json", body)
+	if err != nil {
+		t.Fatalf("Unexpected error: %v", err)
+	}
+	if res.StatusCode != http.StatusOK {
+		t.Fatalf("Expected status ok, got: %d", res.StatusCode)
+	}
+
+}
