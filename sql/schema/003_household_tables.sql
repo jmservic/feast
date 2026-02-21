8eef5880a514ac1af7cdb31b52205e721ecd6b23
Sacriflces <Jmhservice@gmail.com>
2026-02-21 14:01:05 -0500

    Started working on the household create stored procedure

--- sql/schema/003_household_tables.sql
+++ sql/schema/003_household_tables.sql
@@ -2,6 +2,7 @@
 CREATE TABLE households (
 	id UUID PRIMARY KEY,
 	created_at TIMESTAMP NOT NULL, 
+	updated_at TIMESTAMP NOT NULL,
 	name TEXT NOT NULL
 );
 
@@ -20,6 +21,41 @@ CREATE TABLE household_members (
 	user_id UUID REFERENCES users (id)
 );
 
+
+-- procedures
+-- +goose StatementBegin
+CREATE PROCEDURE create_household (name TEXT, userId UUID) AS $$
+DECLARE
+	household_id UUID := gen_random_uuid();
+	member_name TEXT := (SELECT name FROM users WHERE id = userId); 
+BEGIN 
+
+	INSERT INTO households ( id, created_at, updated_at, name )
+    VALUES
+	(
+		household_id,
+		NOW(), 
+		NOW(),
+		name
+	);
+
+	INSERT INTO household_members ( id, name, created_at, updated_at, role, household_id, user_id )
+	VALUES
+	(
+		gen_random_uuid(),
+		member_name,
+		NOW(),
+		NOW(),
+		1,
+		household_id,
+		userId
+	);
+	
+	--SELECT * FROM households WHERE id = household_id;
+END;
+$$ LANGUAGE plpgsql;
+-- +goose StatementEnd
+
 -- initial data
 INSERT INTO household_roles (name) 
 VALUES
@@ -31,3 +67,4 @@ VALUES
 DROP TABLE household_members;
 DROP TABLE household_roles;
 DROP TABLE households;
+DROP PROCEDURE create_household;
