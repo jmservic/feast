43d2516473ff1b452959b29a7f5464d6f612af2e
Sacriflces <Jmhservice@gmail.com>
2026-04-22 20:08:09 -0400

    added nosec to json file

--- cmd/server/json.go
+++ cmd/server/json.go
@@ -11,7 +11,7 @@ func respondWithError(w http.ResponseWriter, code int, msg string, err error) {
 		log.Println(err)
 	}
 	if code > 499 {
-		log.Printf("Responding with 5XX error: %s", msg)
+		log.Printf("Responding with 5XX error: %s", msg) // #nosec G706
 	}
 	type errorResponse struct {
 		Error string `'json:"error"`
