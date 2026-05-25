5247d0e2ce431417470e311a933b007d9fa91687
Sacriflces <Jmhservice@gmail.com>
2026-05-25 13:24:31 -0400

    updated the create household member integration test, and created the get household members integration test

--- integration_tests/household_member_test.go
+++ integration_tests/household_member_test.go
@@ -8,7 +8,7 @@ import (
 	"testing"
 )
 
-func TestCreateHouseholdMember(t *testing.T) {
+func TestCreateHouseholdMember(t *testing.T) { // what about creating a member from an user that's already in another household?
 	// arrange
 	helpers.LoadDotEnv()
 	owner := UserInfo{
@@ -20,6 +20,11 @@ func TestCreateHouseholdMember(t *testing.T) {
 		name:     "cassidy",
 		email:    "Cass@example.com",
 		password: "kalina",
+	}
+	otherOwner := UserInfo{
+		name:     "joey",
+		email:    "Joey@example.com",
+		password: "fAcE-PaIn",
 	}
 
 	householdName := "Service family"
@@ -43,19 +48,42 @@ func TestCreateHouseholdMember(t *testing.T) {
 	res = client.CreateHousehold(loginResponse.Token, householdName)
 	householdCreationResponse := helpers.GetResponseObject[dto.HouseholdResponse](t, res, http.StatusCreated)
 
+	// create other household
+	res = client.CreateUser(otherOwner.name, otherOwner.email, otherOwner.password)
+	createOtherOwnerResponse := helpers.GetResponseObject[dto.UserCreateResponse](t, res, http.StatusCreated)
+
+	res = client.LoginUser(otherOwner.email, otherOwner.password)
+	otherOwnerLoginResponse := helpers.GetResponseObject[dto.UserLoginResponse](t, res, http.StatusOK)
+
+	res = client.CreateHousehold(otherOwnerLoginResponse.Token, "Miller family")
+	res.Body.Close()
+
 	testCases := []struct {
 		testName, memberName string
 		userId               *uuid.UUID
+		status               int
+		memberDiff           int
 	}{
 		{
-			testName:   "Create User without a User Id",
+			testName:   "Create User without an User Id",
 			memberName: "Johnny Jr.",
 			userId:     nil,
+			status:     http.StatusCreated,
+			memberDiff: 1,
 		},
 		{
-			testName:   "Create User with a User Id",
+			testName:   "Create User with an User Id",
 			memberName: "Cassidy",
 			userId:     &createUserResponse.Id,
+			status:     http.StatusCreated,
+			memberDiff: 1,
+		},
+		{
+			testName:   "Create User with an User Id in another household",
+			memberName: "Joey",
+			userId:     &createOtherOwnerResponse.Id,
+			status:     http.StatusInternalServerError,
+			memberDiff: 0,
 		},
 	}
 
@@ -68,19 +96,24 @@ func TestCreateHouseholdMember(t *testing.T) {
 			// act
 			res = client.CreateHouseholdMember(loginResponse.Token, testCase.memberName,
 				householdCreationResponse.Id, testCase.userId)
+			res.Body.Close()
 
 			// assert
-			if res.StatusCode != http.StatusCreated {
-				t.Fatalf("Expected status created for member creation, got: %d", res.StatusCode)
+			if res.StatusCode != testCase.status {
+				t.Fatalf("Expected status created for member creation, got: %d.", res.StatusCode)
 			}
 
 			//Check members after
 			res = client.GetHouseholdMembers(loginResponse.Token, householdCreationResponse.Id)
 			updatedMembers := helpers.GetResponseObject[[]dto.HouseholdMemberResponse](t, res, http.StatusOK)
 
-			if len(updatedMembers) != len(originalMembers)+1 {
+			if len(updatedMembers) != len(originalMembers)+testCase.memberDiff {
 				t.Fatalf("Expected length of updated members list to be %d, but got %d",
-					len(originalMembers)+1, len(updatedMembers))
+					len(originalMembers)+testCase.memberDiff, len(updatedMembers))
+			}
+
+			if testCase.memberDiff == 0 { //we don't expect a new member
+				return
 			}
 
 			foundMember := false
@@ -123,7 +156,30 @@ func TestCreateHouseholdMember(t *testing.T) {
 }
 
 func TestUpdateHouseholdMember(t *testing.T) {
+	// arrange
+	/*helpers.LoadDotEnv()
+	owner := UserInfo{
+		name:     "jonathan",
+		email:    "Jon@example.com",
+		password: "very-secret",
+	}
+	nonMember := UserInfo{
+		name:     "cassidy",
+		email:    "Cass@example.com",
+		password: "kalina",
+	}
+
+	householdName := "Service family"
+
+	feastUrl := helpers.GetFeastURL()
+	t.Cleanup(func() { helpers.ResetDatabase(feastUrl) })
+	client := dto.NewClient(t, feastUrl)
+	*/
+
+	//cases, update name, user id, role.. actually kind of need to do the other ones before I complete this one..
+	// I need users at different role levels, so I can test the failing cases as well.
 	t.FailNow()
+
 }
 
 func TestGetHouseholdMember(t *testing.T) {
@@ -131,7 +187,62 @@ func TestGetHouseholdMember(t *testing.T) {
 }
 
 func TestGetHouseholdMembers(t *testing.T) {
-	t.FailNow()
+	helpers.LoadDotEnv()
+	owner := UserInfo{
+		name:     "jonathan",
+		email:    "Jon@example.com",
+		password: "very-secret",
+	}
+
+	additionalMembers := []string{"shawn", "sha'dimond", "kamyah"}
+
+	householdName := "Service family"
+	feastUrl := helpers.GetFeastURL()
+	t.Cleanup(func() { helpers.ResetDatabase(feastUrl) })
+	client := dto.NewClient(t, feastUrl)
+
+	// create owner
+	res := client.CreateUser(owner.name, owner.email, owner.password)
+	if res.StatusCode != http.StatusCreated {
+		t.Fatalf("Expected status created, got: %d", res.StatusCode)
+	}
+	res.Body.Close()
+
+	// login
+	res = client.LoginUser(owner.email, owner.password)
+	loginResponse := helpers.GetResponseObject[dto.UserLoginResponse](t, res, http.StatusOK)
+
+	//create household
+	res = client.CreateHousehold(loginResponse.Token, householdName)
+	householdCreationResponse := helpers.GetResponseObject[dto.HouseholdResponse](t, res, http.StatusCreated)
+
+	//create other members
+	for _, name := range additionalMembers {
+		res = client.CreateHouseholdMember(loginResponse.Token, name, householdCreationResponse.Id, nil)
+		res.Body.Close()
+		if res.StatusCode != http.StatusCreated {
+			t.Fatalf("Expected household member %v to be created, received response of %d", name, res.StatusCode)
+		}
+	}
+
+	// act finally
+	res = client.GetHouseholdMembers(loginResponse.Token, householdCreationResponse.Id)
+	members := helpers.GetResponseObject[[]dto.HouseholdMemberResponse](t, res, http.StatusOK)
+
+	// assert
+	for _, name := range additionalMembers {
+		found := false
+		for _, member := range members {
+			if member.Name == name {
+				found = true
+				break
+			}
+		}
+
+		if !found {
+			t.Fatalf("Expected to find %v in the household members, but did not", name)
+		}
+	}
 }
 
 func TestDeleteHouseholdMember(t *testing.T) {
